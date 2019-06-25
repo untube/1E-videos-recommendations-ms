a2271e59cc5a6327b27a0366914b7ccd7443a2e9
Diego Araque <daaraquem@unal.edu.co>
2019-06-24 23:30:29 -0500

    Api and models

--- service/models.py
+++ service/models.py
@@ -6,7 +6,7 @@ class UserPreferences(models.Model):
     id_category = models.IntegerField(null=False)
     counter = models.IntegerField(default=1)
 class VideoStatistics(models.Model):
-    id_video = models.CharField(null=False, max_length=200)
+    id_video = models.CharField(max_length=50, null=False)
     id_category = models.IntegerField(null=False)
     sumCalification = models.IntegerField(default=0)
     calicationsCount = models.IntegerField(default=1)

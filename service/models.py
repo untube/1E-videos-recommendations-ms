4efec1b906290dec41571b4cc0084a3630c2fe00
Diego Araque <daaraquem@unal.edu.co>
2019-06-27 04:02:54 -0500

    categoryId int to string

--- service/models.py
+++ service/models.py
@@ -3,11 +3,11 @@ from django.db import models
 # Create your models here.
 class UserPreferences(models.Model):
     id_user = models.IntegerField(null=False)
-    id_category = models.IntegerField(null=False)
+    id_category = models.CharField(max_length=50, null=False)
     counter = models.IntegerField(default=1)
 class VideoStatistics(models.Model):
     id_video = models.CharField(max_length=50, null=False)
-    id_category = models.IntegerField(null=False)
+    id_category = models.CharField(max_length=50, null=False)
     sumCalification = models.IntegerField(default=0)
     calicationsCount = models.IntegerField(default=1)
     num_views = models.IntegerField(default=1)
